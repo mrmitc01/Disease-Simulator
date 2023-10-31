6bca9543b807b6c6ea0e6a640c779ba6f674d264
James Nikolic <98557269+jimanikolic@users.noreply.github.com>
2023-10-31 16:02:27 -0400

    gui

---
+++ Disease.py
@@ -0,0 +1,100 @@
+import math
+class Disease:
+    def __init__(self, trans_type, trans_param, rect_width, rect_height):
+        self.trans_type = trans_type
+        self.trans_param = trans_param
+
+        self.rectangle = {'width': rect_width, 'height': rect_height}
+        self.pointA = [0, 0]
+        self.pointB = [0, self.rectangle['height']]
+        self.pointC = [self.rectangle['width'], self.rectangle['height']]
+        self.pointD = [self.rectangle['width'], 0]
+        #print("Starting Rectangle Width:",self.rectangle['width'],"Height:", self.rectangle['height'])
+        print(f"Rectangle Coordinates BEFORE run:", self.pointA, self.pointB, self.pointC, self.pointD)
+
+    def translate(self):    
+        if self.trans_type == 'translation':
+            self.pointA[0] += self.trans_param
+            self.pointB[0] += self.trans_param
+            self.pointC[0] += self.trans_param
+            self.pointD[0] += self.trans_param
+            #print(f"Rectangle AFTER X Translation: Width = {abs(self.pointC[0]-self.pointB[0])}, Height = {self.pointC[1]}")
+            print(f"Rectangle Coordinates AFTER translation:", self.pointA, self.pointB, self.pointC, self.pointD)
+            return(self.pointA, self.pointB, self.pointC, self.pointD)
+
+    def rotate(self):
+        if self.trans_type == 'rotation':
+            xb, yb = self.pointB
+            xc, yc = self.pointC
+            xd, yd = self.pointD
+            radians = self.trans_param * 3.141592653589793/180
+            nxb = (math.cos(radians) * (xb)) - (math.sin(radians) * (yb))
+            nxc = math.cos(radians) * (xc) - math.sin(radians) * (yc)
+            nxd = math.cos(radians) * (xd) - math.sin(radians) * (yd)
+            self.pointB[0] = round(nxb)
+            self.pointC[0] = round(nxc)
+            self.pointD[0] = round(nxd)
+            #print(f"Rectangle AFTER X Rotation: Width = {abs(self.pointC[0]-self.pointB[0])}, Height = {self.pointC[1]}")
+            print(f"Rectangle Coordinates AFTER rotation:", self.pointA, self.pointB, self.pointC, self.pointD)
+            return(self.pointA, self.pointB, self.pointC, self.pointD)
+
+    def shear(self):
+        if self.trans_type == 'shear':
+            nxa = self.pointA[0] + self.trans_param * self.pointA[1]
+            nxb = self.pointB[0] + self.trans_param * self.pointB[1]
+            nxc = self.pointC[0] + self.trans_param * self.pointC[1]
+            nxd = self.pointD[0] + self.trans_param * self.pointD[1]
+            self.pointA[0] = round(nxa)
+            self.pointB[0] = round(nxb)
+            self.pointC[0] = round(nxc)
+            self.pointD[0] = round(nxd)
+
+            #print(f"Rectangle AFTER X Shear: Width = {abs(self.pointC[0]-self.pointB[0])}, Height = {self.pointC[1]}")
+            print(f"Rectangle Coordinates AFTER shear:", self.pointA, self.pointB, self.pointC, self.pointD)
+            return(self.pointA, self.pointB, self.pointC, self.pointD)
+
+    def scale(self):
+        if self.trans_type == 'scale':
+            nxa = self.pointA[0] * self.trans_param 
+            nxb = self.pointB[0] * self.trans_param 
+            nxc = self.pointC[0] * self.trans_param
+            nxd = self.pointD[0] * self.trans_param
+            self.pointA[0] = round(nxa)
+            self.pointB[0] = round(nxb)
+            self.pointC[0] = round(nxc)
+            self.pointD[0] = round(nxd)
+            #print(f"Rectangle AFTER X Translation: Width = {abs(self.pointC[0]-self.pointB[0])}, Height = {self.pointC[1]}")
+            print(f"Rectangle Coordinates AFTER scale:", self.pointA, self.pointB, self.pointC, self.pointD)
+            return(self.pointA, self.pointB, self.pointC, self.pointD)
+
+
+if __name__ == "__main__":
+
+    #information to be changed as needed:
+
+    #Rectangle size defs
+    rect_width = 60
+    rect_height = 40
+
+    #by x units
+    translation_param = 2
+    #by degrees
+    rotation_param = 60
+    #by shear factor
+    shear_param = 4
+    #by scale factor
+    scale_param = 1.5
+
+
+    #Enter in the format trans_type, trans_param, rect_width, rect_height
+    disease = Disease('translation', translation_param, rect_width, rect_height)
+    disease.translate()
+
+    disease = Disease('rotation', rotation_param, rect_width, rect_height)
+    disease.rotate()
+    
+    disease = Disease('shear', shear_param, rect_width, rect_height)
+    disease.shear()
+
+    disease = Disease('scale', scale_param, rect_width, rect_height)
+    disease.scale()
---
+++ GUI.py
@@ -0,0 +1,150 @@
+from Disease import *
+from PyQt6.QtGui     import *
+from PyQt6.QtWidgets import *
+import sys
+from skimage import io
+from skimage import data
+from skimage.color import rgb2gray
+from matplotlib import pyplot as plt
+
+imgky = io.imread('croppednewmap.png')
+#grayky = rgb2gray(imgky)
+print(type(imgky))
+print(imgky.size)
+print(imgky.shape)
+io.imshow(imgky)
+plt.show()
+
+rect_width = 60
+rect_height = 40
+
+class DrawingApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        #Initializing widget stuff
+        central_widget = QWidget(self)
+        self.setCentralWidget(central_widget)
+
+        layout = QVBoxLayout()
+
+        self.canvas = CanvasWidget(central_widget)
+        layout.addWidget(self.canvas)
+
+        button_layout = QHBoxLayout()
+
+        #Create buttons with their functions
+        self.translate_button = QPushButton("PlaceHolder0", central_widget)
+        #self.translate_button.clicked.connect(self.translation)
+
+        self.rotate_button = QPushButton("PlaceHolder1", central_widget)
+        #self.rotate_button.clicked.connect(self.rotation)
+
+        self.shear_button = QPushButton("PlaceHolder2", central_widget)
+        #self.shear_button.clicked.connect(self.shear)
+
+        self.scale_button = QPushButton("PlaceHolder3", central_widget)
+        #self.scale_button.clicked.connect(self.scale)
+
+        button_layout.addWidget(self.translate_button)
+        button_layout.addWidget(self.rotate_button)
+        button_layout.addWidget(self.shear_button)
+        button_layout.addWidget(self.scale_button)
+
+        #Create text box for input param
+        label = QLabel("Enter Disease parameters:", central_widget)
+
+        self.text_box = QLineEdit(central_widget)
+
+        layout.addLayout(button_layout)
+        layout.addWidget(label)
+        layout.addWidget(self.text_box)
+
+        central_widget.setLayout(layout)
+        self.setGeometry(100, 100, 500, 400)
+
+    def translation(self):
+        text = self.text_box.text()
+        if text:
+            param = int(text)
+        else:
+            param = 0
+        disease = Disease('translation', param, rect_width, rect_height)
+        points = disease.translate()
+        self.canvas.setPoints(points)
+
+    def rotation(self):
+        text = self.text_box.text()
+        if text:
+            param = int(text)
+        else:
+            param = 0
+        Disease = Disease('rotation', param, rect_width, rect_height)
+        points = Disease.rotate()
+        self.canvas.setPoints(points)
+
+    def shear(self):
+        text = self.text_box.text()
+        if text:
+            param = int(text)
+        else:
+            param = 0
+        Disease = Disease('shear', param, rect_width, rect_height)
+        points = Disease.shear()
+        self.canvas.setPoints(points)
+
+    def scale(self):
+        text = self.text_box.text()
+        if text:
+            param = int(text)
+        else:
+            param = 0
+        Disease = Disease('scale', param, rect_width, rect_height)
+        points = Disease.scale()
+        self.canvas.setPoints(points)
+
+class CanvasWidget(QWidget):
+    #Keep a list of points so that they can be updated
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.rect_drawn = False
+        self.points = []
+
+    #Send points to be drawn
+    def setPoints(self, points):
+        self.points = points
+        self.rect_drawn = True
+        self.update()
+
+class CanvasWidget(QWidget):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.rect_drawn = False
+        self.points = []
+
+    def setPoints(self, points):
+        self.points = points
+        self.rect_drawn = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.rect_drawn:
+            #After a button is clicked
+            painter = QPainter(self)
+
+        else:
+            #Before a button is clicked
+            painter = QPainter(self)
+            pixmap = QPixmap("croppednewmap.png")
+
+            painter.drawPixmap(self.rect(), pixmap)
+            
+
+def main():
+    app = QApplication(sys.argv)
+    window = DrawingApp()
+    window.show()
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
