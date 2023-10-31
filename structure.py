6889c3575ee757741ca4306307d84af98039e850
JacobRG <jacobgambrell@gmail.com>
2023-10-31 15:20:48 -0400

    Structure file

---
+++ structure.py
@@ -0,0 +1,29 @@
+from enum import Enum
+
+# STRUCTURE
+class Disease:
+    def __init__(self, name, infection, recovery, mortality):
+        self.name = name             # Disease name
+        self.infection = infection   # Probability of infection when adjacent to an infected individual
+        self.recovery = recovery     # Number of turns for an infected individual to recover
+        self. mortality = mortality  # Probability of death for an infected individual
+
+class State(Enum):
+    SUSCEPTIBLE = "S"
+    INFECTED = "I"
+    RECOVERED = "R"
+    DEAD = "D"
+
+class Statistics:
+    def __init__(self):
+        self.susceptible_count = 0
+        self.infected_count = 0
+        self.recovered_count = 0
+        self.dead_count = 0
+
+class Region:
+    def __init__(self, name, population, land_area, population_density):
+        self.name = name
+        self.populaton = population
+        self.land_area = land_area
+        self.populaton_density = population_density
