e818ba11703ac6cc5d171d335a1b10fe22fce0c6
jimanikolic <jimanikolic@gmail.com>
2023-11-16 14:57:16 -0500

    updated GUI using Image Segmentation

--- GUI.py
+++ GUI.py
@@ -1,131 +1,80 @@
-"""
-Sets up a GUI using PyQt6 for a drawing application. Loads an image and provides functionality for
-translating, rotating, shearing, and scaling a rectangle on a canvas.
-"""
-
-from Disease import *
-from PyQt6.QtGui import *
-from structure import *
-from PyQt6.QtGui import *
-from PyQt6.QtWidgets import *
-import sys
+from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow, QVBoxLayout, QWidget, QPushButton, QColorDialog
+from PyQt6.QtGui import QPixmap, QImage, QPainter, QColor
+import os
 from skimage import io, color
-from matplotlib import pyplot as plt
 import numpy as np
 
-#lightredregion = io.imread('region1.json')
-imgky = io.imread('croppednewmap.png')
-image = color.rgba2rgb(imgky)
-
-# Represents a GUI application for a drawing app.
-class DrawingApp(QMainWindow):
-    def __init__(self):
+class KentuckyViewer(QMainWindow):
+    def __init__(self, segments):
         super().__init__()
 
-        statisticsXCoord = 10
-        infectedLabelYCoord = 350
-        deadLabelYCoord = 365
-        recoveredLabelYCoord = 380
+        self.segments = segments
+        self.initUI()
 
-        # Initializing widget stuff
-        central_widget = QWidget(self)
-        self.setCentralWidget(central_widget)
+    def initUI(self):
+        self.central_widget = QWidget(self)
+        self.setCentralWidget(self.central_widget)
 
-        layout = QVBoxLayout()
+        self.layout = QVBoxLayout()
 
-        self.canvas = CanvasWidget(central_widget)
-        layout.addWidget(self.canvas)
+        self.canvas_label = QLabel(self)
+        self.layout.addWidget(self.canvas_label)
 
-        button_layout = QHBoxLayout()
+        self.buttons_layout = QVBoxLayout()
 
-        #Create buttons with their functions
-        self.runButton = QPushButton("Run", central_widget)
-        self.runButton.clicked.connect(self.run)
-        # Create buttons with their functions
-        self.diseaseType0Button = QPushButton("DiseaseType0", central_widget)
-        # self.translate_button.clicked.connect(self.translation)
+        self.region_widgets = []
 
-        self.diseaseType1Button = QPushButton("DiseaseType1", central_widget)
-        # self.rotate_button.clicked.connect(self.rotation)
+        for i, segment in enumerate(self.segments):
+            region_widget = QPushButton(f"Region {i + 1}", self)
+            region_widget.clicked.connect(lambda _, idx=i: self.update_color(idx))
+            self.region_widgets.append(region_widget)
+            self.buttons_layout.addWidget(region_widget)
 
-        self.diseaseType2Button = QPushButton("DiseaseType3", central_widget)
-        # self.shear_button.clicked.connect(self.shear)
+        self.layout.addLayout(self.buttons_layout)
 
-        self.diseaseType3Button = QPushButton("DiseaseType4", central_widget)
-        # self.scale_button.clicked.connect(self.scale)
+        self.central_widget.setLayout(self.layout)
 
-        #button_layout.addWidget(self.diseaseType0Button)
-        button_layout.addWidget(self.diseaseType1Button)
-        button_layout.addWidget(self.diseaseType2Button)
-        button_layout.addWidget(self.diseaseType3Button)
+        self.setGeometry(100, 100, 800, 600)
+        self.setWindowTitle('Kentucky Map Viewer')
 
-        self.infectedLabel = QLabel("Total Infected: " + str(totalInfected), central_widget)
-        self.infectedLabel.move(statisticsXCoord, infectedLabelYCoord)
-        self.deadLabel = QLabel("Total Dead: " + str(totalDead), central_widget)
-        self.deadLabel.move(statisticsXCoord, deadLabelYCoord)
-        self.recoveredLabel = QLabel("Total Recovered: " + str(totalRecovered), central_widget)
-        self.recoveredLabel.move(statisticsXCoord, recoveredLabelYCoord)
+        self.update_canvas()
 
-        # Create text box for input param
-        label = QLabel("Enter Disease parameters:", central_widget)
+    def update_color(self, region_index):
+        # Update the color of the specific region dynamically
+        region = self.segments[region_index]
+        region[:, :, :3] = np.array([255, 0, 0])  # Set color to red
+        io.imsave(f'region_{region_index + 1}.png', region)
 
-        self.text_box = QLineEdit(central_widget)
-        layout.addLayout(button_layout)
+        # Update the canvas to reflect the changes
+        self.update_canvas()
 
-        layout.addWidget(label)
-        layout.addWidget(self.text_box)
+    def update_canvas(self):
+        # Combine the regions into the full image of Kentucky
+        composite_image = np.zeros_like(self.segments[0])
+        for region in self.segments:
+            composite_image += region
 
-        central_widget.setLayout(layout)
-        self.setGeometry(100, 100, 600, 600)
-
-
-    #Define what you want buttons to do
-    def run(self):
-        text = self.text_box.text()
-        if text:
-            param = int(text)
-        else:
-            param = 0
-        self.canvas.turnRed(self, region2, param)
-
-class CanvasWidget(QWidget):
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.rect_drawn = False
-        self.points = []
-
-    def turnRed(self, region, param):
-        self.region = region
-        for i in param:
-            for row in region:
-                for pixel in row:
-                    if np.allclose(pixel, [1,1,1]):
-                        pixel[1] = 0
-                        pixel[2] = 0
-                        pixel[0] = pixel[0] + 0.1
-                        plt.imshow(region)
-                        plt.show()
-                        self.update()
-
-    def paintEvent(self, event):
-        if self.rect_drawn:
-            #After a button is clicked
-            painter = QPainter(self)
-            pixmap = QPixmap("region_2.png")
-            painter.drawPixmap(self.rect(), pixmap)
-
-        else:
-            #Before a button is clicked
-            painter = QPainter(self)
-            pixmap = QPixmap("out_put_regions\\region_2.png")
-            painter.drawPixmap(self.rect(), pixmap)
-            
+        # Display the composite image
+        io.imsave('combined_kentucky.png', composite_image)
+        pixmap = QPixmap('combined_kentucky.png')
+        self.canvas_label.setPixmap(pixmap)
 
 def main():
-    app = QApplication(sys.argv)
-    window = DrawingApp()
-    window.show()
-    sys.exit(app.exec())
+    app = QApplication([])
 
-if __name__ == "__main__":
+    # Replace 'path_to_segments' with the actual path to your segment images directory
+    segment_directory = 'output_regions'
+
+    # Get a list of segment paths
+    segment_paths = [os.path.join(segment_directory, filename) for filename in os.listdir(segment_directory) if filename.endswith('.png')]
+
+    # Load segments
+    segments = [io.imread(segment_path) for segment_path in segment_paths]
+
+    window = KentuckyViewer(segments)
+    window.show()
+
+    app.exec()
+
+if __name__ == '__main__':
     main()
